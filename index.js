1c594ffb2a22bfff91c61a526edae5fee04a583b
Emmanue <kioxmanu7@gmail.com>
2023-10-18 12:34:18 +0300

    assignemtn done

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+// Function to check if there is a pair of numbers in an array that adds up to a target number
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Nested loop to check each pair of numbers
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      // If the pair adds up to the target, return true
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  // If no pair is found, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // Should return true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // Should return true
+console.log(hasTargetSum([1, 2, 5], 4)); // Should return false
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
